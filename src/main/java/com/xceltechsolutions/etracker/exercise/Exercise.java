9bf44702ca65cf3eb40d78a36fd03fae8129c84d
dieudonne96 <dedeosembej@gmail.com>
2024-09-08 19:59:38 +0200

    init commit

---
+++ src/main/java/com/xceltechsolutions/etracker/exercise/Exercise.java
@@ -0,0 +1,22 @@
+package com.xceltechsolutions.etracker.exercise;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.PositiveOrZero;
+
+import java.time.LocalDateTime;
+
+public record Exercise(
+        Integer id,
+        @NotEmpty
+        String nameOfExercise,
+        String description,
+        @NotEmpty
+        String duration,
+        @PositiveOrZero
+        Integer numberOfSets,
+        @PositiveOrZero
+        Integer numberOfReps,
+        LocalDateTime timeCreated,
+        LocalDateTime timeUpdated
+) {
+}
